1ee40164625daa7324d5f435082da1b593ed3d5b
WahajAli14 <wahajali98@gmail.com>
2025-04-29 18:29:39 +0500

    updated docker compose, created pdf task in worker service

--- worker/app/dispatcher.py
+++ worker/app/dispatcher.py
@@ -1,6 +1,7 @@
 import json
 from app.tasks.print_tasks import process_printing_task
 from app.tasks.email_task import handle_email_task
+from app.tasks.generate_pdf_file import generate_pdf_file
 
 def dispatch_task(body):
     data = json.loads(body)
@@ -12,5 +13,7 @@ def dispatch_task(body):
         process_printing_task(payload)
     elif task_type == "email":
         handle_email_task(payload)
+    elif task_type == "generate_pdf":
+        generate_pdf_file(payload)
     else:
         raise ValueError(f"Unknown task_type: {task_type}")
---
+++ worker/app/tasks/generate_pdf_file.py
@@ -0,0 +1,22 @@
+# app/tasks/pdf_generator.py
+from fpdf import FPDF
+import os
+
+def generate_pdf_file(payload):
+    title = payload.get("title", "Untitled")
+    content = payload.get("content", "")
+
+    # Ensure output folder exists
+    os.makedirs("generated_pdfs", exist_ok=True)
+
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, title, ln=True, align='C')
+    pdf.multi_cell(0, 10, content)
+
+    file_name = f"{title.replace(' ', '_')}.pdf"
+    output_path = os.path.join("generated_pdfs", file_name)
+    pdf.output(output_path)
+
+    print(f"✅ PDF generated at {output_path}")
